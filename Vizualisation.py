9b23b1283470f9c56070c40ebbb7495873967648
heraud_b <baptiste.heraud@epitech.eu>
2020-01-27 14:44:25 +0100

    Requirement

--- Vizualisation.py
+++ Vizualisation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import sys
 import csv
-
+from numpy import genfromtxt
 
 
 if __name__ == "__main__":
+    data = genfromtxt('data.csv', delimiter=',')
     print ("ok")
     sys.exit(0)
