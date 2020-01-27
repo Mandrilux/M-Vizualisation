a0456756ab387d6d56d88b1ce95b945332596a24
heraud_b <baptiste.heraud@epitech.eu>
2020-01-27 15:11:18 +0100

    IO FILE

--- Vizualisation.py
+++ Vizualisation.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 import sys
 import csv
+import os
+import errno
 from numpy import genfromtxt
 
 
+def get_name_csv():
+    dir = os.listdir('./')
+    for x in range(0, len(dir)):
+        if len(dir[x]) > 4 and dir[x][len(dir[x]) - 4:len(dir[x])] == ".csv":
+            return (dir[x])
+    return ("")
+
 if __name__ == "__main__":
-    data = genfromtxt('data.csv', delimiter=',')
-    print ("ok")
+    file_data = get_name_csv()
+    print ("Found DataFile %s"% (file_data))
+    try:
+        data = genfromtxt(file_data, delimiter=',')
+    except IOError as e :
+        print (str(e))
+        sys.exit(42)
     sys.exit(0)
