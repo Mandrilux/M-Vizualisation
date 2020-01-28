90ad9997803b2861025c443d6b87e90529eb818a
heraud_b <baptiste.heraud@epitech.eu>
2020-01-28 16:14:54 +0100

    GetType

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # M-Vizualisation
 pip3 freeze > requirements.txt
 
+This binary can found any csv.
+
+For execute : ./Vizualisation.py
+
 By antoine.iamurri@epitech.eu and Baptiste.heraud@epitech.eu
 
--- Vizualisation.py
+++ Vizualisation.py
@@ -15,6 +15,9 @@ def get_name_csv():
             return dir[x]
     return ""
 
+def getDateWithType(data , type):
+    
+    return data
 
 if __name__ == "__main__":
     file_data = get_name_csv()
@@ -28,6 +31,20 @@ if __name__ == "__main__":
     except IOError as e:
         print(str(e))
         sys.exit(42)
+    print ("What type do you want ?")
+    print ("Only man : press M")
+    print ("Only girl : press F")
+    print ("All : Any key")
+    type = input("Please enter any Key : ").split(' ')[0]
+    print (type)
+
+    if type == "H":
+        print ("Loading man data")
+        data = getDateWithType(data, 1)
+    elif type == "F":
+        print ("Loading female data")
+        data = getDateWithType(data, 2)
+
     print("Parsing dataFile %s" % file_data)
     ScatterViz(data)
     sys.exit(0)
