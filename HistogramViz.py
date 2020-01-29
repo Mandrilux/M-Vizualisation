b15b1e31f619a7cd6edfddcdb9ed3a1ee560ea91
Antoine Iamurri <antoine.iamurri@gmail.com>
2020-01-29 17:28:04 +0100

    distplots

--- HistogramViz.py
+++ HistogramViz.py
@@ -7,7 +7,7 @@ def HistogramViz(data, flag):
     format_data = FormatData(data)
 
     print("Generate graph : please wait")
-    fig = px.histogram(format_data, x="age", y="size", color="place")
+    fig = px.histogram(format_data, x="age", y="size", color="place", marginal="box")
 
     name_file = "histogram_" + flag + ".html"
     fig.write_html(name_file, auto_open=True)
--- ScatterViz.py
+++ ScatterViz.py
@@ -7,7 +7,7 @@ def ScatterViz(data, flag):
     format_data = FormatData(data)
 
     print("Generate graph : please wait")
-    fig = px.scatter(format_data, x="age", y="place", color="place", opacity=0.1, size="size")
+    fig = px.scatter(format_data, x="age", y="size", color="place", opacity=0.1, size="size", trendline='lowess')
 
     name_file = "scatter_" + flag + ".html"
     fig.write_html(name_file, auto_open=True)
